7b40a1caf58f4d6c09c075826bf3c3635833282a
smerrill2 <spencer19merrill@gmail.com>
2024-10-17 11:28:45 -0500

    updated api's

--- src/lib/supabaseServerClient.ts
+++ src/lib/supabaseServerClient.ts
@@ -2,7 +2,7 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseUrl = process.env.SUPABASE_URL!;
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY!;
 
 if (!supabaseUrl || !supabaseServiceKey) {
